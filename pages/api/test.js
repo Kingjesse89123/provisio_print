583c80a70164f00e4a058dcd6e234adf642d22f9
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2022-12-23 12:31:09 -0800

    WERE IDIOTS!!
    seriously, we are

---
+++ pages/api/test.js
@@ -0,0 +1,51 @@
+import {useState} from "react";
+
+export default function handler(req, res) {
+
+    if (req.method === 'GET') {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://5nn73jb7.directus.app/items/print_jobs/5bbae1b7-68db-439b-ab53-fa0f593d6034", requestOptions)
+            .then(response => response.json())
+            .then(result => res.status(200).json(result.data.print_job))
+            .catch(error => console.log('error', error));
+    }
+    else if(req.method ===  'POST'){
+        console.log(req)
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://5nn73jb7.directus.app/items/restaurants?filter[id][_eq]=2860d46e-efd4-49bc-861f-462bfc8ec667", requestOptions)
+            .then(response => response.json())
+            .then(result => result.data[0].printer_queue ? res.status(200).json({
+                jobReady: true,
+                mediaTypes: ["text/plain"]
+            }) : res.status(200).json('Printer queue empty'))
+            .catch(error => console.log('error', error));
+    } else if(req.method === 'DELETE'){
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "printer_queue": []
+        });
+
+        const requestOptions = {
+            method: 'PATCH',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://5nn73jb7.directus.app/items/restaurants/2860d46e-efd4-49bc-861f-462bfc8ec667", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
+}
