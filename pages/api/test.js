40870185afc723d6e62b31905e1058c4492b47d9
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2022-12-23 15:39:04 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -16,8 +16,8 @@ export default function handler(req, res) {
             .then(response => response.json())
             .then((result)=>{
                 fetch(`https://5nn73jb7.directus.app/assets/${result.data.print_job}`, requestOptions)
-                    .then(response => response.json())
-                    .then(result => res.status(200).json(result))
+                    .then(response2 => response2.json())
+                    .then(result2 => res.status(200).json(result2))
                     .catch(error => console.log('error', error));
             })
             .catch(error => console.log('error', error));
