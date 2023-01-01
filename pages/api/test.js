d2fa76f227a8ab36feca0412f67c27e222cbedba
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2023-01-01 15:19:35 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -7,13 +7,12 @@ export default function handler(req, res) {
             method: 'GET',
             redirect: 'follow'
         };
-        console.log('pog')
         res.setHeader("Content-Type", "application/vnd.star.starprnt")
 
-        fetch("https://5nn73jb7.directus.app/items/print_jobs/5bbae1b7-68db-439b-ab53-fa0f593d6034", requestOptions)
+        fetch("https://5nn73jb7.directus.app/items/restaurants?filter[id][_eq]=2860d46e-efd4-49bc-861f-462bfc8ec667&fields=*.*", requestOptions)
             .then(response => response.json())
             .then((result)=>{
-                fetch(`https://5nn73jb7.directus.app/assets/${result.data.print_job}`, requestOptions)
+                fetch(`https://5nn73jb7.directus.app/assets/${result.data.printer_queue.print_job}`, requestOptions)
                     .then((response2)=> response2.text() )
                     .then(result2 => res.status(200).send(result2))
                     .catch(error => console.log('error', error));
@@ -32,7 +31,7 @@ export default function handler(req, res) {
             .then(result => result.data[0].printer_queue[0] ? res.status(200).json({
                 jobReady: true,
                 mediaTypes: ["text/plain"]
-            })+console.log("postsuceed") : res.status(200).json('Printer queue empty')+ console.log('it stopped here'))
+            }): res.status(200).json('Printer queue empty'))
             .catch(error => console.log('error', error));
         console.log('post done')
     } else if(req.method === 'DELETE'){
