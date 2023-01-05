a0fcaf3e7adc6ac3c74d2aa2dd36bae51ecc5df2
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2023-01-05 13:51:49 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,34 +1,30 @@
 
-var hi
+var mac
 export default function handler(req, res) {
     if(req.method ===  'POST') {
-        console.log('posting')
-        hi = req.body.printerMAC
-        console.log(req.body)
+        mac = req.body.printerMAC
         const requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
-
-        fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${hi}`, requestOptions)
-            .then(response => response.json())
-            .then(result => result.data[0].printer_queue[0] ? res.status(200).json({
-                jobReady: true,
-                mediaTypes: ["text/plain"]
-            }) : res.status(200).json('Printer queue empty'))
-            .catch(error => console.log('error', error));
+        if(req.body.status === '29 a 2 0 0 0 0 2 0 0 0 0' && req.body.printingInProgress === false) {
+            fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}`, requestOptions)
+                .then(response => response.json())
+                .then(result => result.data[0].printer_queue[0] ? res.status(200).json({
+                    jobReady: true,
+                    mediaTypes: ["text/plain"]
+                }) : res.status(200).json('Printer queue empty'))
+                .catch(error => console.log('error', error));
+        }
     }
         if (req.method === 'GET') {
-        console.log(hi)
-        console.log('getting')
-        console.log(req.body)
         const requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
         res.setHeader("Content-Type", "application/vnd.star.starprnt")
 
-        fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${hi}&fields=*.*`, requestOptions)
+        fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions)
             .then(response => response.json())
             .then((result)=>{
                 fetch(`https://5nn73jb7.directus.app/assets/${result.data[0].printer_queue[0].print_job}`, requestOptions)
@@ -39,26 +35,30 @@ export default function handler(req, res) {
             .catch(error => console.log('error', error));
     }
         if(req.method === 'DELETE'){
-        console.log('deleting')
-        console.log(req.body)
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
-        const raw = JSON.stringify({
-            "printer_queue": []
-        });
-
+            const requestOptions2 = {
+                method: 'GET',
+                redirect: 'follow'
+            };
+            const raw = JSON.stringify({
+                "printer_queue": []
+            });
         const requestOptions = {
             method: 'PATCH',
             headers: myHeaders,
             body: raw,
             redirect: 'follow'
         };
-
-        fetch(`https://5nn73jb7.directus.app/items/restaurants/2860d46e-efd4-49bc-861f-462bfc8ec667`, requestOptions)
-            .then(response => response.text())
-            .then(result => res.status(200).json("Print Job Done"))
-            .catch(error => console.log('error', error));
-    }
-
+            fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions2)
+                .then(response => response.json())
+                .then((result)=>{
+                    fetch(`https://5nn73jb7.directus.app/items/restaurants/${result.data.id}`, requestOptions)
+                        .then((response2)=> response2.text() )
+                        .then(result2 => res.status(200).send("Print Job Done"))
+                        .catch(error => console.log('error', error));
+                })
+                .catch(error => console.log('error', error));
+        }
 }
