e116152f4d37e3e510c32ec590cf6a1afaf4ac48
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2023-01-05 16:16:41 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -11,7 +11,8 @@ export default function handler(req, res) {
             method: 'GET',
             redirect: 'follow'
         };
-        if(req.body.status == '29 a 2 0 0 0 0 2 0 0 0 0' && req.body.printingInProgress === false) {
+        
+        if(req.body.printingInProgress === false) {
             console.log(true)
             fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}`, requestOptions)
                 .then(response => response.json())
