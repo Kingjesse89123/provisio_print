f2ed50297d9e2f374d97afad7ba9f3e1db6be948
Kingjesse89123 <115125795+Kingjesse89123@users.noreply.github.com>
2023-01-14 13:53:46 -0800

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -10,7 +10,7 @@ export default function handler(req, res) {
         };
         if (req.body.printingInProgress === false && req.body.status.toString().charAt(0) === '2') {
             console.log(true)
-            fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}`, requestOptions)
+            fetch(`https://blueplate.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}`, requestOptions)
                 .then(response => response.json())
                 .then(result => result.data[0].printer_queue[0] ? res.status(200).json({
                     jobReady: true,
@@ -29,10 +29,10 @@ export default function handler(req, res) {
         };
         res.setHeader("Content-Type", "application/vnd.star.starprnt")
 
-        fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions)
+        fetch(`https://blueplate.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions)
             .then(response => response.json())
             .then((result) => {
-                fetch(`https://5nn73jb7.directus.app/assets/${result.data[0].printer_queue[0].print_job}`, requestOptions)
+                fetch(`https://blueplate.directus.app/assets/${result.data[0].printer_queue[0].print_job}`, requestOptions)
                     .then((response2) => response2.text())
                     .then(result2 => res.status(200).send(result2))
                     .catch(error => console.log('error', error));
@@ -61,10 +61,10 @@ export default function handler(req, res) {
             redirect: 'follow'
         };
 
-        fetch(`https://5nn73jb7.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions2)
+        fetch(`https://blueplate.directus.app/items/restaurants?filter[printer_mac][_eq]=${mac}&fields=*.*`, requestOptions2)
             .then((response) => response.json())
             .then(result => {
-                fetch(`https://5nn73jb7.directus.app/items/print_jobs/${result.data[0].printer_queue[0].id}`, requestOptions)
+                fetch(`https://blueplate.directus.app/items/print_jobs/${result.data[0].printer_queue[0].id}`, requestOptions)
                     .then((response2) => response2.text())
                     .then((result2) => {
                         res.status(200).send("Print Job Done")
